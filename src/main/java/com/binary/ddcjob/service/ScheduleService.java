efa63d47d4bc01ed09a9495b841d5cf9817f2f59
hwang <ilyoung6016@gmail.com>
2023-04-18 14:00:10 +0900

    fixed webhook url

--- src/main/java/com/binary/ddcjob/service/ScheduleService.java
+++ src/main/java/com/binary/ddcjob/service/ScheduleService.java
@@ -2,7 +2,9 @@ package com.binary.ddcjob.service;
 
 import com.binary.ddcjob.utils.SlackUtil;
 import com.slack.api.Slack;
+import com.slack.api.model.block.LayoutBlock;
 import com.slack.api.webhook.WebhookResponse;
+import kotlin.collections.ArrayDeque;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -15,11 +17,12 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.List;
 
-import static com.slack.api.model.block.Blocks.asBlocks;
-import static com.slack.api.model.block.Blocks.section;
+import static com.slack.api.model.block.Blocks.*;
 import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
 import static com.slack.api.model.block.composition.BlockCompositions.plainText;
+import static com.slack.api.model.block.element.BlockElements.button;
 import static com.slack.api.webhook.WebhookPayloads.payload;
 
 @Slf4j
@@ -37,14 +40,22 @@ public class ScheduleService {
         log.info("START >>>>>>>>> ");
         Document doc = Jsoup.connect(ddcUrl).get();
         Elements tr =  doc.select(".bbs_default > tbody > tr");
-
         String toDate = LocalDate.now().toString();
+
+
         for (int index = tr.size()-1; index > 0; index--) {
             log.info("toDay={}", toDate);
             log.info("jobInformation={}", tr.get(index));
 
+            String contentText = tr.get(index).select("td > a").text();
+
             if(toDate.equals(tr.get(index).lastElementChild().text())) {
-                SlackUtil.send(webhookUrl, "새로운 동두천시 일자리가 업데이트 되었습니다.\n" + ddcUrl);
+                List<LayoutBlock> layoutBlocks = new ArrayDeque<>();
+                layoutBlocks.add(header(header -> header.text(plainText("🔔동두천시 신규 일자리 알림"))));
+                layoutBlocks.add(divider());
+                layoutBlocks.add(section(section -> section.text(markdownText(contentText)).accessory(button(btn -> btn.text(plainText("홈페이지")).url(ddcUrl).actionId("button-action")))));
+
+                SlackUtil.send(webhookUrl, layoutBlocks);
             }
         }
         log.info("END >>>>>>>>> ");
--- src/main/java/com/binary/ddcjob/utils/SlackUtil.java
+++ src/main/java/com/binary/ddcjob/utils/SlackUtil.java
@@ -1,10 +1,13 @@
 package com.binary.ddcjob.utils;
 
 import com.slack.api.Slack;
+import com.slack.api.model.block.Blocks;
+import com.slack.api.model.block.LayoutBlock;
 import com.slack.api.webhook.WebhookResponse;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.util.List;
 
 import static com.slack.api.model.block.Blocks.asBlocks;
 import static com.slack.api.model.block.Blocks.section;
@@ -17,9 +20,9 @@ public class SlackUtil {
 
     private static Slack slack = Slack.getInstance();
 
-    public static void send(String webhookUrl, String content) throws IOException {
+    public static void send(String webhookUrl, List<LayoutBlock> layoutBlocks) throws IOException {
         try {
-            slack.send(webhookUrl, payload(p -> p.blocks(asBlocks(section(section -> section.text(markdownText(content)))))));
+            slack.send(webhookUrl, payload(p -> p.blocks(layoutBlocks)));
         } catch (Exception e) {
             log.error("Slack Error Message={}", e.getCause());
         }
